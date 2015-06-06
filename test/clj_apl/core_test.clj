38b7df4f7a10007deda18732e3c3bbb430836871
Chris LaRose <cjlarose@gmail.com>
2015-06-05 20:45:53 -0700

    cjl - Add test for logical not

--- test/clj_apl/core_test.clj
+++ test/clj_apl/core_test.clj
@@ -6,4 +6,9 @@
   (testing "reciprocal"
     (is (= 0.5 (divide 2.0)))
     (is (= 0.25 (divide 4.0)))
-    (is (= 0.125 (divide 8.0)))))
+    (is (= 0.125 (divide 8.0))))
+  (testing "not"
+    (is (= 1 (logical-not 0)))
+    (is (= 0 (logical-not 1)))
+    (is (thrown-with-msg? ArithmeticException #"Domain error" (logical-not 5)))
+    (is (thrown-with-msg? ArithmeticException #"Domain error" (logical-not -1)))))
