c359a6f355e254beca6a2835dde6f4837752147d
Chris LaRose <cjlarose@gmail.com>
2015-06-05 21:08:00 -0700

    cjl - Add test for monadic power fn

--- test/clj_apl/core_test.clj
+++ test/clj_apl/core_test.clj
@@ -2,6 +2,13 @@
   (:require [clojure.test :refer :all]
             [clj-apl.core :refer :all]))
 
+(defn float=
+  "http://gettingclojure.wikidot.com/cookbook:numbers"
+  ([x y] (float= x y 0.00001))
+  ([x y epsilon]
+     (let [scale (if (or (zero? x) (zero? y)) 1 (Math/abs x))]
+       (<= (Math/abs (- x y)) (* scale epsilon)))) )
+
 (deftest monadic-fns
   (testing "reciprocal"
     (is (= 0.5 (divide 2.0)))
@@ -11,4 +18,9 @@
     (is (= 1 (logical-not 0)))
     (is (= 0 (logical-not 1)))
     (is (thrown-with-msg? ArithmeticException #"Domain error" (logical-not 5)))
-    (is (thrown-with-msg? ArithmeticException #"Domain error" (logical-not -1)))))
+    (is (thrown-with-msg? ArithmeticException #"Domain error" (logical-not -1))))
+  (testing "powers of euler's constant"
+    (is (= 1.0 (power 0.0)))
+    (is (= Math/E (power 1.0)))
+    (is (float= 0.36787944117144233 (power -1.0)))
+    (is (float= 735.0951892419727 (power 6.6)))))
