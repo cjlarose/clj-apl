1d2722dc6aded1bc097c8bfb3b61e32201f894af
Chris LaRose <cjlarose@gmail.com>
2015-06-06 00:31:32 -0700

    cjl - Tests use new interface

--- test/clj_apl/core_test.clj
+++ test/clj_apl/core_test.clj
@@ -1,6 +1,6 @@
 (ns clj-apl.core-test
   (:require [clojure.test :refer :all]
-            [clj-apl.core :refer :all]))
+            [clj-apl.core :as apl]))
 
 (defn float=
   "http://gettingclojure.wikidot.com/cookbook:numbers"
@@ -11,26 +11,32 @@
 
 (deftest monadic-fns
   (testing "reciprocal"
-    (is (= 0.5 (divide 2.0)))
-    (is (= 0.25 (divide 4.0)))
-    (is (= 0.125 (divide 8.0))))
+    (is (= 0.5 (apl/÷ 2.0)))
+    (is (= 0.25 (apl/÷ 4.0)))
+    (is (= 0.125 (apl/÷ 8.0))))
   (testing "not"
-    (is (= 1 (logical-not 0)))
-    (is (= 0 (logical-not 1)))
-    (is (thrown-with-msg? ArithmeticException #"Domain error" (logical-not 5)))
-    (is (thrown-with-msg? ArithmeticException #"Domain error" (logical-not -1))))
+    (is (= 1 (apl/∼ 0)))
+    (is (= 0 (apl/∼ 1)))
+    (is (thrown-with-msg? ArithmeticException #"Domain error" (apl/∼ 5)))
+    (is (thrown-with-msg? ArithmeticException #"Domain error" (apl/∼ -1))))
   (testing "powers of euler's constant"
-    (is (= 1.0 (power 0.0)))
-    (is (= Math/E (power 1.0)))
-    (is (float= 0.36787944117144233 (power -1.0)))
-    (is (float= 735.0951892419727 (power 6.6))))
+    (is (= 1.0 (apl/⋆ 0.0)))
+    (is (= Math/E (apl/⋆ 1.0)))
+    (is (float= 0.36787944117144233 (apl/⋆ -1.0)))
+    (is (float= 735.0951892419727 (apl/⋆ 6.6))))
   (testing "natural log"
-    (is (= 0.0 (logarithm 1.0)))
-    (is (float= 2.0149030205422647 (logarithm 7.5)))
-    (is (= 1.0 (logarithm Math/E))))
+    (is (= 0.0 (apl/⍟ 1.0)))
+    (is (float= 2.0149030205422647 (apl/⍟ 7.5)))
+    (is (= 1.0 (apl/⍟ Math/E))))
   (testing "sign function"
-    (is (= 1.0 (signum-multiply 1.0)))
-    (is (= 1.0 (signum-multiply 234.4)))
-    (is (= -1.0 (signum-multiply -564.2)))
-    (is (= -1.0 (signum-multiply -1.2)))
-    (is (= 0.0 (signum-multiply 0)))))
+    (is (= 1.0 (apl/× 1.0)))
+    (is (= 1.0 (apl/× 234.4)))
+    (is (= -1.0 (apl/× -564.2)))
+    (is (= -1.0 (apl/× -1.2)))
+    (is (= 0.0 (apl/× 0.0))))
+  (testing "identity"
+    (is (= 1.0 (apl/+ 1.0)))
+    (is (= 234.4 (apl/+ 234.4)))
+    (is (= -564.2 (apl/+ -564.2)))
+    (is (= -1.2 (apl/+ -1.2)))
+    (is (= 0.0 (apl/+ 0.0)))))
