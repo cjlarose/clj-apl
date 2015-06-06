5f977e4e8f8e8e51901407718a997a20190e18c2
Chris LaRose <cjlarose@gmail.com>
2015-06-05 20:35:38 -0700

    cjl - Begin tests

--- test/clj_apl/core_test.clj
+++ test/clj_apl/core_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [clj-apl.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest monadic-fns
+  (testing "reciprocal"
+    (is (= 0.5 (divide 2.0)))
+    (is (= 0.25 (divide 4.0)))
+    (is (= 0.125 (divide 8.0)))))
