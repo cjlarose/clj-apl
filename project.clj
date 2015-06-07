90c23e91ab46c13813409b580e8738fb8d7bb975
Chris LaRose <cjlarose@gmail.com>
2015-06-06 19:30:29 -0700

    cjl - Add coverage tool

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [net.mikera/core.matrix "0.35.0"]]
+  :plugins [[lein-cloverage "1.0.6"]]
   :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.9.0"]]}})
